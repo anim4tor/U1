9d807fd3a551d14368a954e99bfb80a8c4c2d2e9
anim4tor <jiri.klusak@gmail.com>
2026-06-19 17:12:57 +0200

    Homepage

--- site/components/templates/Home/Services/index.php
+++ site/components/templates/Home/Services/index.php
@@ -1,10 +1,11 @@
 <section class="solutions color__invert" data-tabs="hoverable">
 	<div class="bg radius absolute inset__stretch" theme="dark"></div>
 	<div class="grid__2 place__start-stretch mobile:grid__1 mobile:inner-t__10 mobile:gap__2 relative inner-b__5" >
+		<div class="span__2 inner__1 inner-y__2" data-scroll>
+			<h2 class="l" data-reveal-text><span>Services</span></h2>
+		</div>
 		<div class="sticky top__0 h__100v grid place__start-space-between gap__1 inner__1 inner-t__2" data-scroll>
-			<div class="" data-scroll>
-				<h1 class="font__size__default ff__body s upper" data-reveal-text>(<span>Solutions</span>)</h1>
-			</div>
+			
 			<div class="sticky top__10 grid__stack place__start-start no__overflow img__radius" data-pane-container>
 				<?php foreach (collection('Solutions') as $solution) : ?>
 					<div class="no__overflow" data-pane="service-<?= $solution->slug() ?>" data-tab-reveal >
