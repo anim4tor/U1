717ca63858ef612da77e205fa132caa9c8fc1fb1
anim4tor <jiri.klusak@gmail.com>
2026-06-29 16:03:02 +0200

    Post

--- site/components/templates/Post/index.php
+++ site/components/templates/Post/index.php
@@ -2,13 +2,12 @@
 	<div data-scroll class="z__1 intro__header inner-b__3 place__stretch-stretch grid__4 mobile:grid__1 mobile:h__auto inner__1 mobile:inner-t__10 mobile:gap__2 relative">
 		<div class="h__10"></div>
 		<div class="span__4 grid place__stretch-stretch">
-			<div class="span__4 border__top inner-t__05 grid__4 place__space-between-stretch">
+			<div class="span__4 border__top inner-t__05 grid grid__post gap__2 place__space-between-stretch">
 				<p class="grid place__start-start font__size__large">
 					<span class="upper ">(Blog post)</span>
 					<span class="op__4"><?= $page->date()->toDate('Y-m-d') ?></span>
 				</p>
-				<div></div>
-				<div class="span__2 -wrap-l__5">
+				<div class="">
 					<h1 class="font__size__2 xs">
 						<div data-reveal-text=""><?= $page->title() ?></div>
 					</h1>
@@ -16,26 +15,39 @@
 			</div>
 		</div>
 	</div>
-	<div class="inner-x__1 inner-b__5">
+	<div class="inner-x__1">
 		<?php if ($cover = $page->cover()->toFile()) : ?>
 		<div class="intro__cover radius grid" data-scroll ><?= snippet('atoms/Image', ['img' => $cover, 'parallax' => 2, 'reveal' => false, 'css' => 'overlay__bottom']) ?></div>
 		<?php endif ?>
 	</div>
 </section>
 
-<section class="about" theme="invert">
-	<div data-scroll class="grid__3 gap__2 mobile:grid__1 inner__1 mobile:inner-x__1 inner-y__2">
-		<div class="span__2 grid place__start-start gap__3 mobile:inner-x__0">
-			<h2 data-reveal-text="lines" class="font__size__4"><?= $page->intro()->inline() ?></h2>
+<section class="details" theme="invert">
+
+	<div data-scroll class="place__stretch-stretch grid gap__2 inner-x__1 inner-t__2 inner-b__5">
+		<div class="grid span__2 gap__2 grid__post">
+			<div class=""></div>
+			<h2 data-reveal-text="lines" class="font__size__4 m"><?= $page->excerpt()->inline() ?></h2>
 		</div>
-		
-	</div>
-	<div data-scroll class="grid__3 gap__2 mobile:grid__1 inner__1 mobile:inner-x__1 inner-y__2">
-		<div></div>
-		<div class=" grid place__start-start gap__3 mobile:inner-x__0">
-			<p data-reveal-text="lines" class=""><?= $page->details()->inline() ?></p>
-		</div>
-		
+		<?php $start = false; ?>
+		<?php $end = false; ?>
+		<?php foreach ($page->details()->toBlocks() as $block): ?>
+			<?php $end = $block->isLast() ? true : false ?>
+
+			<?php if ($block->type() == 'heading' && in_array($block->level(), ['h2','h3'])) : ?>
+				<?= $start ? '</div>' : null ?>
+				<div id="<?= Str::slug($block->text()->inline()) ?>" class="span__2 grid grid__post gap__2 inner-y__1 border__top">	
+					<div class="-wrap-r__5 inner-r__10">
+						<h3 class="font__size__5"><?= $block->text() ?></h3>
+					</div>
+					<div class="grid gap__1 inner-r__10">
+				<?php $start = true ?>
+			<?php else : ?>
+				<?php snippet('blocks/'.$block->type(), ['block' => $block]) ?>
+			<?php endif; ?>
+
+			<?= $start && $end ? '</div></div>' : null ?>
+		<?php endforeach ?>
 	</div>
 </section>
 
