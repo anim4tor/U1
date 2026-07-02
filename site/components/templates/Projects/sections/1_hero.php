abb203b9920ca6ba04e8fb33483db95fafa9f8c8
anim4tor <jiri.klusak@gmail.com>
2026-07-02 22:53:40 +0200

    Works filter

--- site/components/templates/Projects/sections/1_hero.php
+++ site/components/templates/Projects/sections/1_hero.php
@@ -5,8 +5,29 @@
 		<div class="span__4 inner-t__05 border__top grid__4 place__space-between-stretch">
 			<div class="span__3">
 				<div class="flex align__start gap__02 inner-y__02">
-					<?= snippet('molecules/Header', ['header' => $page->hero(), 'type' => ['heading']]) ?>
-					<?= snippet('atoms/Text', ['text' => '('.collection('Projects')->count().')', 'reveal' => true, 'css' => 'font__size__4' ]) ?>
+					
+					<?php if (!empty($filterBy)) : ?>
+						<?php 
+						// 1. Find the clean display text for the active slug
+						$activeText = '';
+						foreach ($tags as $tagItem) {
+							if ($tagItem['slug'] === $filterBy) {
+								$activeText = $tagItem['text'];
+								break;
+							}
+						}
+						?>
+						<?= snippet('atoms/Heading', [
+							'text'   => $activeText, 
+							'level'  => 'h1',
+							'reveal' => true
+						]) ?>
+
+					<?php else : ?>
+						<?= snippet('molecules/Header', ['header' => $page->hero(), 'type' => ['heading']]) ?>
+					<?php endif ?>
+
+					<?= snippet('atoms/Text', ['text' => '('.$projects->count().')', 'reveal' => true, 'css' => 'font__size__4' ]) ?>
 				</div>
 			</div>
 			<div class="flex justify__end">
